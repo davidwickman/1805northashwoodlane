0d907764b7bc836c622c7254f9535ddd94cbeac5
David Wickman <david.wickman@sherwin.com>
2014-04-01 16:42:25 -0400

    initial commit - contingent

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+	<meta name="description" content="1805 North Ashwood Lane - A Colonial Home in the Heisley Park Community">
+
+	<meta name="keywords" content="1805 North Ashwood Lane, North Ashwood, Heisley Park, Painesville Home" />
+
+	<meta name="viewport" content="width=device-width">
+
+	<title>1805 North Ashwood Lane - A Colonial Home in the Heisley Park Community</title>
+	
+	<link rel="shortcut icon" href="/favicon.ico">
+
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300italic' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="author" href="https://plus.google.com/100212435987136443968/posts">
+
+	<script src="js/libs/modernizr-2.5.0.min.js"></script>
+</head>
+<body>
+	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6. chromium.org/developers/how-tos/chrome-frame-getting-started -->
+	<!--[if lt IE 9]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="#chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+	<header>
+		<div class="house-ribbon">
+			<div class="ribbon-open-house image-gallery-ribbon">
+				Contingent
+			</div>
+		</div>
+		<div class="bg-image bg-image-header"></div>
+	</header>
+	
+	<div role="main">
+		<?php /* <img src="./img/web_logo.png" alt="Wickman Studios" class="logo center" /> */ ?>
+		
+		<div class="button_center">
+			<h1 class="call-out">1805 North Ashwood Lane</h1>
+			<?php /* <h2 class="all-out"><strong>Open Sunday 12 - 2!</strong></h2> */ ?>
+			<h2 class="open-banner"><strong>Contingent</strong></h2>
+			<h2>A Colonial Home in the Heisley Park Community</h2>
+			<p>A unique and rare find with a great, open layout; these are not your typical builder upgrades. This four year young home has after-market remodeling, including dark wood floors throughout the entire first floor, large deck out the sliding glass doors, and a brand new kitchen! Forget the after build expenses of installing your own yard, landscaping, and deck. This home has these items all completed and the craftsmanship is world class! The 19'x16' deck with privacy fence is perfect for dining al fresco, if you must leave the gourmet, chef-inspired kitchen! You will not find this gourmet kitchen in any other home in the neighborhood. The kitchen has $50k in remodeling including a six burner Wolf Stove, 42" counter-depth KitchenAid fridge, solid maple cabinets, a huge farm sink, & Restoration Hardware lighting throughout! Make an offer before it's gone! You can't build one like it!</p>
+			<p>Enter this gorgeous colonial & find beautiful dark wood floors throughout the first floor! The foyer opens up to the formal dining room. Passing the tucked away 1/2 bath, the large family room is centered around a gorgeous stone fireplace, but immediately your attention will turn to look into the gourmet, chef inspired kitchen. The $50k updates include 36 Wolf Range & 42 KitchenAid Fridge (both stay), solid maple cabinets, Restoration Hardware lighting, 30 Kraus apron sink with restaurant style faucet, Amish made butcher block counters & more! An eat in kitchen is just past the 8' L shaped island, with vaulted ceilings & sliding door leading to the 19x16 deck with privacy fence. Upstairs you will find a large master bedroom with walk in closet, full bath with double sinks & soaking tub/shower combo. The upstairs is completed with 2 more bedrooms, a full bath, & a huge, open loft. The lower level of the home is a wide open canvas, plumbed for a full bath, ready to make your own!</p>
+			<a href="mailto:cj@ohiohomeseller.com?cc=dave@wickmanstudios.com" class="btn btn-block btn-hg btn-inverse">Contact</a>
+			<a href="assets/kitchen-2012-06.pdf" class="btn btn-block btn-hg btn-inverse">The Kitchen Layout (2012)</a>
+			<a href="assets/deck-2013-09.pdf" class="btn btn-block btn-hg btn-inverse">19'x16' Deck Plan (2013)</a>
+			<a href="assets/home-layout-Florence-1805-layout.pdf" class="btn btn-block btn-hg btn-inverse">The Floor Plans (2010)</a>
+			<a href="inc/NEOHREXBuyerFull.pdf" class="btn btn-block btn-hg btn-inverse">Full Buyer's Flier</a>
+			<a href="http://greatbuycleveland.kwrealty.com/listing/mlsid/388/propertyid/3476169/" class="btn btn-block btn-hg btn-inverse">More Info!</a>
+		</div>
+
+		<div class="img-list">
+			<img src="img/photos/001.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/002.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/003.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/004.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/005.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/006.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/007.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/008.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/009.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/010.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/011.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/012.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/013.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/014.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/015.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/016.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/017.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/018.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/019.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/020.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/021.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/022.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/023.jpg" alt="1805 North Ashwood Lane">
+			<img src="img/photos/024.jpg" alt="1805 North Ashwood Lane">
+		</div>
+
+	</div>
+	<footer class="button_center">
+		<a href="mailto:cj@ohiohomeseller.com?cc=dave@wickmanstudios.com" class="btn btn-block btn-hg btn-inverse">Contact</a>
+		<a href="assets/kitchen-2012-06.pdf" class="btn btn-block btn-hg btn-inverse">The Kitchen Layout (2012)</a>
+		<a href="assets/deck-2013-09.pdf" class="btn btn-block btn-hg btn-inverse">19'x16' Deck Plan (2013)</a>
+		<a href="assets/home-layout-Florence-1805-layout.pdf" class="btn btn-block btn-hg btn-inverse">The Floor Plans (2010)</a>
+		<a href="inc/NEOHREXBuyerFull.pdf" class="btn btn-block btn-hg btn-inverse">Full Buyer's Flier</a>
+		<a href="http://greatbuycleveland.kwrealty.com/listing/mlsid/388/propertyid/3476169/" class="btn btn-block btn-hg btn-inverse">More Info!</a>
+	</footer>
+</body>
+</html>
